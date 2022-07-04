dc44751ab1c1b973022df49f535c867289b11c72
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-04 17:52:41 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  namespace :v1, defaults: { format: 'json' } do 
+    get 'greetings', to: 'greetings#index'
+  end 
+  get '*path', to: 'static_pages#index'
 end
